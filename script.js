9bd85b3b90f3dfeb125e8391e90438c7d174f1c4
samrat9xm <primeminister9xm@gmail.com>
2024-01-13 17:20:42 +0600

    made the animation with js instead of css

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const number = document.querySelector(".number");
+const stroke = document.querySelector("circle");
 let counter = 0;
-
+let circleFiller = 472;
 setInterval(function () {
-    if (counter == 65) {
+    if (counter === 65) {
         clearInterval();
     } else {
         counter++;
+        circleFiller -= 4.72;
+        stroke.style.strokeDashoffset = circleFiller;
         number.innerText = counter + "%";
     }
-}, 30);
+}, 20);
